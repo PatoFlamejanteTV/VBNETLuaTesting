312c142386eae394ceaf16295e19b867197bf21c
PatoFlamejanteTV <flamejantepato77@gmail.com>
2025-01-07 17:49:08 -0300

    v1.0.0

---
+++ Form1.vb
@@ -0,0 +1,13 @@
+﻿Imports LuaInterface
+
+Public Class Form1
+
+    Public luascripting As New Lua()
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim luaScript As String = TextBox1.Text
+
+        Console.WriteLine("Code: " & luaScript)
+        luascripting.DoString(luaScript)
+    End Sub
+End Class
